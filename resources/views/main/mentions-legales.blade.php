78baae094a804f6c6274ea8602179d3d7d49ec2d
ffindev <freddy.fond@gmail.com>
2024-11-24 21:39:11 +0100

    Ajout de modifications de style

--- resources/views/main/mentions-legales.blade.php
+++ resources/views/main/mentions-legales.blade.php
@@ -8,15 +8,20 @@
 
     <h2 id="pagestart">Vous êtes sur la page mentions-legales.</h2>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore inventore nostrum natus voluptates aperiam? Consequatur, adipisci? Ratione optio molestias amet ipsum excepturi, provident iusto nobis culpa, saepe corrupti facere dolor?
-    Doloremque eligendi itaque recusandae ratione nobis debitis consequuntur molestiae id libero expedita sapiente corrupti, mollitia, nihil voluptatibus perferendis ipsa maxime et error minus distinctio aliquam? Ad similique aperiam ipsam molestiae.
-    Voluptatibus sit laudantium expedita adipisci nemo magni repellat, magnam, quasi commodi repudiandae pariatur natus debitis. Mollitia, officiis assumenda. Consectetur laudantium voluptate nam incidunt corporis quaerat quos velit quibusdam corrupti fuga?
-    Odio, optio reiciendis quia quidem perspiciatis nisi blanditiis commodi quisquam, facere possimus saepe quae iusto adipisci voluptas, provident explicabo nemo vitae. Aspernatur laborum ducimus alias beatae odit! Illo, doloremque deserunt?
-    Molestiae, blanditiis voluptates mollitia perferendis et recusandae dignissimos eligendi modi eveniet dolorum doloremque saepe rerum sapiente, quaerat cum atque amet. Ab animi nemo recusandae voluptate minus odit sint esse est!
-    Magnam laboriosam officiis rem, facere nulla labore repellendus aut reprehenderit ex, unde in? Ea qui repellendus asperiores eaque nemo? Nostrum facere nisi voluptas sunt suscipit vitae magni aut enim dolor!
-    Consequatur, dicta ut dolorem tempore repellat unde minus id odit hic, architecto iste animi ducimus voluptas numquam excepturi sint et quasi incidunt facilis quod. Sit distinctio esse ipsa possimus magnam?
-    Error enim voluptate, dolore ab sequi fuga hic quae deserunt dignissimos facilis architecto, sapiente, iste saepe veritatis qui dicta repellat quis minima esse dolorem itaque. Fugiat tenetur quas incidunt quasi.
-    Ducimus pariatur quaerat commodi accusamus nihil, deserunt eum nostrum sunt ipsum itaque accusantium est harum quia aperiam recusandae hic fugit enim beatae porro, ipsam obcaecati architecto quos cum temporibus. Veritatis!
-    Similique dicta odio voluptatibus doloribus, voluptatum eum id iusto rem suscipit enim expedita asperiores nemo quos cumque officia quisquam labore! Ipsam recusandae earum hic ipsum. Cumque earum aperiam necessitatibus at.</p>
+    <p id="legals">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam voluptas iusto libero praesentium, animi odit cupiditate cum, culpa facere dolorum minima provident odio veritatis unde, minus dolorem exercitationem beatae voluptatem.
+    Fugit, nisi nobis laudantium quidem eos ex eius perspiciatis quod iste, debitis repudiandae? Iusto officiis sequi corporis aliquam debitis rerum dolores quas mollitia nulla, repellat provident, ab dolore deleniti cum?
+    Officiis voluptatem aspernatur beatae quidem? Ex quidem expedita, ab deleniti provident commodi sint asperiores dolorem quae perspiciatis corrupti sit, architecto illo et ut vitae aspernatur. Natus, quo dignissimos! Numquam, deserunt?
+    Repellat ipsam debitis ut laboriosam natus at architecto blanditiis odio et atque possimus sed magnam, quibusdam fuga? Accusantium, aperiam! Omnis odio itaque aliquid repellendus soluta at iure eveniet facere quaerat.
+    Amet aliquid maiores quae recusandae incidunt eos tenetur libero, aut obcaecati minus dicta provident animi laborum. A, dignissimos ut, minima nobis enim iure aliquam dolores saepe molestiae itaque suscipit eius.
+    Blanditiis nam ut tempore deserunt dolorum, natus quos! Corrupti cupiditate repellendus tempore, quos dolor doloremque impedit tenetur laborum, quae commodi, ducimus error quis itaque quidem veritatis vel nemo blanditiis sapiente!
+    Qui, minus, ut beatae commodi velit tenetur consequuntur ipsum pariatur molestias quam cupiditate voluptas exercitationem, similique dolore quae dolores placeat libero? Quidem neque illum fugit quaerat cum distinctio tempore consequuntur.
+    Non odit officia doloremque quidem unde eum est officiis nesciunt veritatis maiores veniam reprehenderit voluptatum aperiam cum excepturi pariatur qui iusto libero ducimus corporis, ab incidunt? Quod quos officiis deleniti.
+    Fuga, enim facere harum id esse consequatur atque quaerat repellat sit totam magni architecto quo maxime perferendis cupiditate quisquam nesciunt ratione dignissimos? Recusandae, quod doloremque autem vitae qui nesciunt veritatis.
+    Eligendi aperiam nesciunt magnam possimus, commodi laboriosam, sunt et minima vel delectus recusandae voluptatum temporibus cum dolorum dicta, ratione vero. Veniam tenetur quae nulla, architecto laborum alias? Praesentium, sunt alias!
+    Laudantium laboriosam aliquid hic odio porro quis tempora fugit, fugiat aut excepturi deleniti nulla vero ipsam facilis ex expedita quisquam ullam quia! Praesentium sed reprehenderit nemo quasi fugiat, qui ab?
+    Id ratione temporibus reiciendis error deserunt incidunt, animi a magni alias, voluptatem consequuntur ex dolore laboriosam repellendus quasi obcaecati perferendis tempora possimus? Impedit quia facere iste at nobis doloribus provident.
+    Unde reiciendis delectus veniam voluptas quod dicta earum, repellendus eos eaque tenetur nam dignissimos fuga. Doloribus labore odio recusandae consequuntur eum deleniti voluptatibus dicta, esse cupiditate, qui tenetur numquam porro?
+    Ex veritatis quo non labore sapiente, accusamus tempore! Ratione placeat, libero corporis assumenda quidem minus reprehenderit sed deserunt dolor. Quos doloremque aliquid aperiam assumenda, nemo corrupti voluptatibus? Vero, suscipit recusandae.
+    Reprehenderit tempora repellat repudiandae voluptates, ex perferendis natus vitae architecto eligendi possimus tempore atque enim libero doloremque reiciendis? Quam reprehenderit hic molestias vero autem dolore enim sed rerum sit. Voluptas!</p>
 
 @endsection
